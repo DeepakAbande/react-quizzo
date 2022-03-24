e72dff1ef65b1656ecab6b7e5fd8602910e20056
DeepakAbande <d7841996820@gmail.com>
2022-03-24 15:57:58 +0530

    prof page updated-2

--- src/Profilepage/proForm.js
+++ src/Profilepage/proForm.js
@@ -6,7 +6,7 @@ import {Link} from 'react-router-dom';
 const ProForm = () => {
     return (
         <>
-
+            <div className="profbg">
             <div className="sidebar">
                 <div className="profile">
                     <img className="pic" alt='img' src="https://images.pexels.com/photos/4925916/pexels-photo-4925916.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" ></img>
@@ -27,48 +27,39 @@ const ProForm = () => {
                         <tr>
                             <td className="llable">First Name </td>
                             {/* <td> <input className="signInput" type="text" name="fname" id="fname" autoComplete="off" /></td> */}
-                            <td id="fname">Deepak</td>
+                            <td id="fname"> <b> Deepak</b></td>
                             <td>Last Name</td>
                             {/* <td><input className="signInput" type="text" name="lname" id="lname" autoComplete="off" /></td> */}
-                            <td id="fname">Abande</td>
+                            <td id="fname"><b>Abande</b></td>
                         </tr>
 
                         <tr>
                             <td className='llable' >Email ID </td>
                             {/* <td> <input className="signInput" type="text" name="email" id="email" autoComplete="off" /></td> */}
-                            <td id="fname">deepakabande26@gmail.com</td>
+                            <td id="fname"><b>deepakabande26@gmail.com</b></td>
                             <td>Phone No</td>
                             {/* <td><input className="signInput" type="text" name="phone" id="phone" autoComplete="off" /></td> */}
-                            <td id="fname">9112820800</td>
+                            <td id="fname"><b>9112820800</b></td>
                         </tr>
 
                         <tr>
                             <td className='llable'>Date of Birth </td>
                             {/* <td> <input className="signInput" type="date" name="Date" id="Date" /></td> */}
-                            <td id="fname">26/05/2001</td>
+                            <td id="fname"><b>26/05/2001</b></td>
                             <td>Gender</td>
                             <td>
-                                <input className="signInput" type="radio" name="Gender" id="male" />Male
-                                <input className="signInput" type="radio" name="Gender" id="female" />Female
+                            <td id="fname"><b>Male</b></td>
+                                {/* <input className="signInput" type="radio" name="Gender" id="male" />Male
+                                <input className="signInput" type="radio" name="Gender" id="female" />Female */}
                             </td>
                         </tr>
-
-                        {/* <tr>
-                            <td className='llable' > Password </td>
-                            <td> <input className="signInput" type="password" name="pwd" id="pwd1" value="Pankaj" /></td>
-                            <td>Show Password</td>
-                            <td><input className="signInput" type="Checkbox" name="pwd" id="pwd2" onclick="myFunction()" /></td>
-                        </tr> */}
 
 
                     </table>
 
-                    {/* <div>
-                        <button className='profileSubmit'>Save</button>
-                    </div> */}
-
                 </div>
             </form>
+            </div>
 
         </>
     )
